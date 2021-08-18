a8c82e8d12287f3ed4983ebc7e6eb05a162dc071
Joseph Edmonds <consultingjde@gmail.com>
2021-08-17 23:05:51 -0700

    first draft

--- src/Components.js/About.js
+++ src/Components.js/About.js
@@ -4,6 +4,11 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Share+Tech&display=swap');
+</style>
+
 const useStyles = makeStyles({
   root: {
     minWidth: '70%',
@@ -17,6 +22,7 @@ const useStyles = makeStyles({
   },
   title: {
     fontSize: 14,
+    fontFamily: 'Share Tech, sans-serif'
   },
 });
 
@@ -27,7 +33,15 @@ function About() {
     <Card className={classes.root}>
       <CardContent>
         <Typography className={classes.title} color="textPrimary" variant='button'>
-          Insert Elevator Pitch here
+          Hello I'm <strong>Joseph</strong> and I would like to tell you a story!...
+          <br />
+          Growing up I wanted to be a doctor for two reasons, I loved learning and I was constantly curious.
+          I did not pursue that route as I made the decision to enter the workforce early starting in customer service.
+          It was a great 10 years in customer service that has left me surprised, empowered, and prepared.
+          Now that I have some experience with people I am wanting to take what I have learned and leverage it in the tech industry.
+          I am currently opening to opportunities in JavaScript, React.js, React Native, Express.js, Node.js and MongoDB.
+          <br />
+          Got a position? Give me an interview and you won't be dissapointed.
         </Typography>
       </CardContent>
     </Card>
