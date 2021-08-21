16d61245d2b60527ab640dcb9e0d2f09e7db2bfd
Joseph Edmonds <consultingjde@gmail.com>
2021-08-20 17:53:24 -0700

    deploy with new navbar

--- src/Components.js/Navbar.js
+++ src/Components.js/Navbar.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
+import format from 'date-fns/format';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -32,8 +33,8 @@ function Navbar() {
     <AppBar className={classes.root}>
       <Toolbar>
         <Typography className={classes.name}>Joseph.Edmonds</Typography>
-        <Link to="section__project" className={classes.sectionLink} color="inherit">Projects</Link>
-        <Link to="section__about" className={classes.sectionLink} color="inherit">Resume</Link>
+        <Typography className={classes.sectionLink}>{format(new Date(), 'MMMM do Y')}</Typography>
+
       </Toolbar>
     </AppBar>
   )
